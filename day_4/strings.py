fe57fba854d9c9cb8982eb7a97b5e3bdf20aa521
Marc Bernardoni <marc.bernardoni@gmail.com>
2022-08-31 20:11:36 +0200

    add day_4

---
+++ day_4/strings.py
@@ -0,0 +1,5 @@
+###########################################
+##  Day 4: 30 Days of python programming  #
+###########################################
+
+# 1 - Concatenate the string 'Thirty', 'Days', 'Of', 'Python' to a single string, 'Thirty Days Of Python'.
