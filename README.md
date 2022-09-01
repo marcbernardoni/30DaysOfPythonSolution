504131a2e12a647ecb952de1d9d503a5318ce336
Marc Bernardoni <marc.bernardoni@gmail.com>
2022-09-01 21:37:31 +0200

    Prepare tasks for day 6 - Tuples

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # 30DaysOfPythonSolution
 
-* Day 01 Introduction
-* Day 02 Variables, Built-in Functions
-* Day 03 Operators
-* Day 04 Strings
+- Day 01 Introduction
+- Day 02 Variables, Built-in Functions
+- Day 03 Operator
+- Day 04 String
+- Day 05 List
+- Day 06 Tuple
---
+++ day_6/tuples-L2.py
@@ -0,0 +1,18 @@
+###########################################
+##  Day 6: 30 Days of python programming  #
+###########################################
+import math
+
+# Exercises: Level 2
+
+nordic_countries = ('Denmark', 'Finland', 'Iceland', 'Norway', 'Sweden')
+
+# 1 - Unpack siblings and parents from family_members
+# 2 - Create fruits, vegetables and animal products tuples. Join the three tuples and assign it to a variable called food_stuff_tp.
+# 3 - Change the about food_stuff_tp tuple to a food_stuff_lt list
+# 4 - Slice out the middle item or items from the food_stuff_tp tuple or food_stuff_lt list.
+# 5 - Slice out the first three items and the last three items from food_staff_lt list
+# 6 - Delete the food_staff_tp tuple completely
+# 7 - Check if an item exists in tuple:
+# a - Check if 'Estonia' is a nordic country
+# b - Check if 'Iceland' is a nordic country
