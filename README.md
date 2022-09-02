2e269c460bc43893eab109279d38fc5bc7e3e5c0
Marc Bernardoni <marc.bernardoni@gmail.com>
2022-09-02 18:06:53 +0200

    Preparation of level 1 to 3 exercises for day 7 of the '30 days of Python' challenge

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # 30DaysOfPythonSolution
 
-- Day 01 Introduction
-- Day 02 Variables, Built-in Functions
-- Day 03 Operator
-- Day 04 String
-- Day 05 List
-- Day 06 Tuple
+- Day 01 : Introduction
+- Day 02 : Variables, Built-in Functions
+- Day 03 : Operator
+- Day 04 : String
+- Day 05 : List
+- Day 06 : Tuple
+- Day 07 : Sets
---
+++ day_07/sets-L1.py
@@ -0,0 +1,18 @@
+###########################################
+##  Day 7: 30 Days of python programming  #
+###########################################
+
+# Exercises: Level 1
+# sets
+it_companies = {'Facebook', 'Google', 'Microsoft',
+                'Apple', 'IBM', 'Oracle', 'Amazon'}
+
+# 1 - Find the length of the set it_companies
+
+# 2 - Add 'Twitter' to it_companies
+
+# 3 - Insert multiple IT companies at once to the set it_companies
+
+# 4 - Remove one of the companies from the set it_companies
+
+# 5 - What is the difference between remove and discard
