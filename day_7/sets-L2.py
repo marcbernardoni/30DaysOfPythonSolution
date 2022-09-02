6ef5597ef30662158edcc3b5ce8e9c29d10c1d2d
Marc Bernardoni <marc.bernardoni@gmail.com>
2022-09-02 18:09:22 +0200

    Change the name of the day_07 folder to day_7

---
+++ day_7/sets-L2.py
@@ -0,0 +1,21 @@
+###########################################
+##  Day 7: 30 Days of python programming  #
+###########################################
+
+# Exercises: Level 2
+A = {19, 22, 24, 20, 25, 26}
+B = {19, 22, 20, 25, 26, 24, 28, 27}
+
+# 1 - Join A and B
+
+# 2 - Find A intersection B
+
+# 3 - Is A subset of B
+
+# 4 - Are A and B disjoint sets
+
+# 5 - Join A with B and B with A
+
+# 6 - What is the symmetric difference between A and B
+
+# 7 - Delete the sets completely
