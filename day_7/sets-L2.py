22e6a6a08ae66529bf4dc9e295714a70fdba9c2d
Marc Bernardoni <marc.bernardoni@gmail.com>
2022-09-03 13:57:17 +0200

    30 Days of Python" Level 2 Exercises Solution for Day 7

--- day_7/sets-L2.py
+++ day_7/sets-L2.py
@@ -7,15 +7,29 @@ A = {19, 22, 24, 20, 25, 26}
 B = {19, 22, 20, 25, 26, 24, 28, 27}
 
 # 1 - Join A and B
+C = A.union(B)
+print(C)
 
 # 2 - Find A intersection B
+print(C)
 
 # 3 - Is A subset of B
+D = A.issubset(B)
+print(D)
 
 # 4 - Are A and B disjoint sets
+E = A.isdisjoint(B)
+print(E)
 
 # 5 - Join A with B and B with A
+F = A.union(B)
+G = B.union(A)
+print(F == G)
 
 # 6 - What is the symmetric difference between A and B
+A.symmetric_difference_update(B)
+print(A)
 
 # 7 - Delete the sets completely
+del A
+del B
